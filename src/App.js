23c1dff667e5ca71413e90a308f7d090d9c36c55
Artak <artak.avanesyan93@gmail.com>
2019-04-09 19:10:08 +0300

    create shop

--- src/App.js
+++ src/App.js
@@ -1,28 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    state = {
+        value: '',
+        search: '',
+        products: []
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3001/products.json')
+            .then(response => response.json())
+            .then(response => {
+                this.setState({
+                    products: response.products
+                })
+            })
+    }
+
+    valueChangeHandler = event => {
+        this.setState({
+            value: event.target.value
+        })
+    }
+
+    handlerKeyPress = event => {
+        if (event.key === 'Enter' && this.state.value) {
+            this.setState(prevState => ({
+                search: prevState.value
+            }))
+            this.setState({
+                value: ''
+            })
+        }
+    }
+
+    search() {
+        return this.state.products.filter((item) => {
+            return item.name.toLowerCase().indexOf(this.state.search.toLowerCase()) > -1
+        })
+    }
+
+    render() {
+        return (
+        <div className="app">
+            <input 
+                type='text'
+                value={this.state.value}
+                onChange={this.valueChangeHandler}
+                onKeyPress={this.handlerKeyPress}
+                placeholder='Product name'
+            />
+            <section>
+            {
+                this.search().length === 0 && this.state.search ? 
+                <p>Products with the same name is missing. Please, try again.</p> 
+                : this.search().map((item) => (
+                    <div key={item.asin + item.name} className='item'>
+                        <a href={item.link} className='name' title={item.name}>{item.name} </a>
+                        <img src={item.img} alt="" width='200' height="200"/>
+                        <p><span className='bold'>Price:</span> {item.price} &#163;</p>
+                    </div>
+                ))
+            }
+            </section>
+        </div>
+        );
+    }
 }
 
 export default App;
